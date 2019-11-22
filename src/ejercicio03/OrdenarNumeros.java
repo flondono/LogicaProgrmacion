7ca8fed9f320837d5e282bd1824cbf6471ba362b
flondono <flondono89@gmail.com>
2019-11-21 21:33:17 -0500

    Se agrega el ejercicio 03, se da solución al ejercicio 03

--- src/ejercicio03/OrdenarNumeros.java
+++ src/ejercicio03/OrdenarNumeros.java
@@ -1,7 +1,49 @@
 package ejercicio03;
 
+import java.util.Scanner;
+
 public class OrdenarNumeros {
     public static void main(String[] args) {
-        
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        int a = entrada.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        int b = entrada.nextInt();
+
+        System.out.print("Ingrese el tercer número: ");
+        int c = entrada.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("El mayor es: " + a);
+            if (b > c) {
+                System.out.println("El número del medio es:" + b);
+                System.out.println("El número menor es: " + c);
+            } else {
+                System.out.println("El número del medio es:" + b);
+                System.out.println("El número menor es: " + c);
+            }
+        } else if (b > a && b > c) {
+            System.out.println("El mayor es: " + b);
+            if (a > c) {
+                System.out.println("El número del medio es:" + a);
+                System.out.println("El número menor es: " + c);
+            } else {
+                System.out.println("El número del medio es:" + c);
+                System.out.println("El número menor es: " + a);
+            }
+        } else {
+            System.out.println("El mayor es: " + c);
+            if (a > b) {
+                System.out.println("El número del medio es:" + a);
+                System.out.println("El número menor es: " + b);
+            } else {
+                System.out.println("El número del medio es:" + b);
+                System.out.println("El número menor es: " + c);
+            }
+        }
+
     }
 }
