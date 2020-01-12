0342bc0516b0a3ab583dedc8510fc26850c47ee0
flondono89@gmail.com <2BieKq8Yn6>
2020-01-12 18:59:43 -0500

    se modifica archivo gitignore

--- src/ejercicio06/NominaSencilla.java
+++ src/ejercicio06/NominaSencilla.java
@@ -1,8 +1,32 @@
 package ejercicio06;
 
+import java.util.Scanner;
+
 public class NominaSencilla {
 
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese nombre: ");
+        String nombre = entrada.nextLine();
+
+        System.out.print("Ingrese número de horas trabajadas: ");
+        int horasTrabajadas = entrada.nextInt();
+
+        System.out.print("Ingrese valor hora: ");
+        double valorHora = entrada.nextDouble();
+
+        System.out.print("Ingrese valor salario mínimo mensual: ");
+        double salarioMinimo = entrada.nextDouble();
+
+        if (horasTrabajadas * valorHora > salarioMinimo) {
+            //System.out.println("Nombre: " + nombre);
+            double salario = horasTrabajadas * valorHora;
+            String aux = String.format("Nombre: %s\nSalario: $%,.2f", nombre, salario);
+            System.out.println(aux);
+        } else {
+            System.out.println("Nombre: " + nombre);
+        }
     }
 }
