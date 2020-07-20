828c61631a9b91a53b7d2d7347861644fc1ada5f
flondono <flondono89@gmail.com>
2020-07-20 13:29:02 -0500

    Se termina ejercicio 07 Descuento

---
+++ src/ejercicio07/Descuento.java
@@ -0,0 +1,6 @@
+package ejercicio07;
+public class Nomina{
+    public static void main(String[] args) {
+        System.out.println("hola1");
+    }
+}
--- src/ejercicio07/Nomina.java
+++
@@ -1,4 +0,0 @@
-package ejercicio07;
-
-public class Nomina {
-}
