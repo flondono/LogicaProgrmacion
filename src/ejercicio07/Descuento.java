3b0048d762b9c2e1e4a4cd5464df41ef41a7c724
flondono <flondono89@gmail.com>
2020-07-20 13:29:32 -0500

    ejercicio 07 descuento

--- src/ejercicio07/Descuento.java
+++ src/ejercicio07/Descuento.java
@@ -1,6 +1,35 @@
 package ejercicio07;
-public class Nomina{
+
+import java.util.Scanner;
+
+public class Descuento {
     public static void main(String[] args) {
-        System.out.println("hola1");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Número de escritorios: ");
+        int numEscritorios = entrada.nextInt();
+
+        //Procedimiento para calcular Descuento
+        double valorTotal = calcularDescuento(numEscritorios);
+        System.out.println("El valor a pagar es: $ " + valorTotal);
+    }
+
+    private static double calcularDescuento(int escritorio) {
+
+        double valorDescuento = 0;
+        final double VALORUNIDAD = 650000;
+
+        if (escritorio < 5) {
+            valorDescuento = (VALORUNIDAD * escritorio) * 0.9;
+            return valorDescuento;
+        } else if (escritorio >= 5 && escritorio < 10) {
+            valorDescuento = (VALORUNIDAD * escritorio) * 0.8;
+            return valorDescuento;
+        } else {
+            valorDescuento = (VALORUNIDAD * escritorio) * 0.6;
+            return valorDescuento;
+        }
+
     }
 }
